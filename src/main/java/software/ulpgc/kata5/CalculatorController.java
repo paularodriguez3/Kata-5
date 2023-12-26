772fe36ec545d0139da76d374ecc0f5c29a06ac1
paularodriguez3 <paularosarodriguezmorales03@gmail.com>
2023-12-26 19:35:37 +0000

    Class CalculatorController created.

---
+++ src/main/java/software/ulpgc/kata5/CalculatorController.java
@@ -0,0 +1,16 @@
+package software.ulpgc.kata5;
+
+import spark.Spark;
+public class CalculatorController {
+
+    private final CalculatorService calculatorService;
+
+    public CalculatorController() {
+        this.calculatorService = new CalculatorService();
+    }
+
+    public void setupRoutes(){
+        Spark.get("/", ((request, response) -> calculatorService.getCalculatorPage()));
+        Spark.post("/calculate", (((request, response) -> calculatorService.calculateFactorial(request))));
+    }
+}
